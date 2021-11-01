309c93bbde68e64acc27eeed9f3c172afdf8cbc7
Luke Braithwaite <lukebraithwaite7@gmail.com>
2021-11-01 13:15:40 +0000

    Initial Commit

---
+++ MyAsthmaUITests/MyAsthmaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyAsthmaUITestsLaunchTests.swift
+//  MyAsthmaUITests
+//
+//  Created by Luke Braithwaite on 01/11/2021.
+//
+
+import XCTest
+
+class MyAsthmaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
